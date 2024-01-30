71cff059a5378d7d740b63e37c7f90cc408ac3f7
Manuel Wessely <m.n.wessely@gmail.com>
2024-01-30 16:39:49 +0100

    configured for rbx

--- examples/MinimalModemGPSExample/MinimalModemGPSExample.ino
+++ examples/MinimalModemGPSExample/MinimalModemGPSExample.ino
@@ -11,7 +11,7 @@
 #include "XPowersLib.h"
 #include "utilities.h"
 
-XPowersPMU  PMU;
+XPowersPMU PMU;
 
 // See all AT commands, if wanted
 #define DUMP_AT_COMMANDS
@@ -22,37 +22,37 @@ XPowersPMU  PMU;
 #include <TinyGsmClient.h>
 #include "utilities.h"
 
-
 #ifdef DUMP_AT_COMMANDS
 #include <StreamDebugger.h>
 StreamDebugger debugger(Serial1, Serial);
-TinyGsm        modem(debugger);
+TinyGsm modem(debugger);
 #else
-TinyGsm        modem(SerialAT);
+TinyGsm modem(SerialAT);
 #endif
 
-float lat2      = 0;
-float lon2      = 0;
-float speed2    = 0;
-float alt2      = 0;
-int   vsat2     = 0;
-int   usat2     = 0;
+float lat2 = 0;
+float lon2 = 0;
+float speed2 = 0;
+float alt2 = 0;
+int vsat2 = 0;
+int usat2 = 0;
 float accuracy2 = 0;
-int   year2     = 0;
-int   month2    = 0;
-int   day2      = 0;
-int   hour2     = 0;
-int   min2      = 0;
-int   sec2      = 0;
-bool  level     = false;
+int year2 = 0;
+int month2 = 0;
+int day2 = 0;
+int hour2 = 0;
+int min2 = 0;
+int sec2 = 0;
+bool level = false;
 
 void setup()
 {
 
     Serial.begin(115200);
 
-    //Start while waiting for Serial monitoring
-    while (!Serial);
+    // Start while waiting for Serial monitoring
+    while (!Serial)
+        ;
 
     delay(3000);
 
@@ -61,28 +61,30 @@ void setup()
     /*********************************
      *  step 1 : Initialize power chip,
      *  turn on modem and gps antenna power channel
-    ***********************************/
-    if (!PMU.begin(Wire, AXP2101_SLAVE_ADDRESS, I2C_SDA, I2C_SCL)) {
-        Serial.println("Failed to initialize power.....");
-        while (1) {
-            delay(5000);
-        }
-    }
-    //Set the working voltage of the modem, please do not modify the parameters
-    PMU.setDC3Voltage(3000);    //SIM7080 Modem main power channel 2700~ 3400V
-    PMU.enableDC3();
+     ***********************************/
+    // if (!PMU.begin(Wire, AXP2101_SLAVE_ADDRESS, I2C_SDA, I2C_SCL))
+    // {
+    //     Serial.println("Failed to initialize power.....");
+    //     while (1)
+    //     {
+    //         delay(5000);
+    //     }
+    // }
+    // Serial.println("PMU Initialized.");
+    // // Set the working voltage of the modem, please do not modify the parameters
+    // PMU.setDC3Voltage(3000); // SIM7080 Modem main power channel 2700~ 3400V
+    // PMU.enableDC3();
 
-    //Modem GPS Power channel
-    PMU.setBLDO2Voltage(3300);
-    PMU.enableBLDO2();      //The antenna power must be turned on to use the GPS function
+    // // Modem GPS Power channel
+    // PMU.setBLDO2Voltage(3300);
+    // PMU.enableBLDO2(); // The antenna power must be turned on to use the GPS function
 
-    // TS Pin detection must be disable, otherwise it cannot be charged
-    PMU.disableTSPinMeasure();
-
+    // // TS Pin detection must be disable, otherwise it cannot be charged
+    // PMU.disableTSPinMeasure();
 
     /*********************************
      * step 2 : start modem
-    ***********************************/
+     ***********************************/
 
     Serial1.begin(115200, SERIAL_8N1, BOARD_MODEM_RXD_PIN, BOARD_MODEM_TXD_PIN);
 
@@ -95,9 +97,11 @@ void setup()
     digitalWrite(BOARD_MODEM_PWR_PIN, LOW);
 
     int retry = 0;
-    while (!modem.testAT(1000)) {
+    while (!modem.testAT(1000))
+    {
         Serial.print(".");
-        if (retry++ > 15) {
+        if (retry++ > 15)
+        {
             // Pull down PWRKEY for more than 1 second according to manual requirements
             digitalWrite(BOARD_MODEM_PWR_PIN, LOW);
             delay(100);
@@ -113,7 +117,7 @@ void setup()
 
     /*********************************
      * step 3 : start modem gps function
-    ***********************************/
+     ***********************************/
 
     //  When configuring GNSS, you need to stop GPS first
     modem.disableGPS();
@@ -165,38 +169,58 @@ void setup()
     delay(500);
 
     // GPS function needs to be enabled for the first use
-    if (modem.enableGPS() == false) {
+    if (modem.enableGPS() == false)
+    {
         Serial.print("Modem enable gps function failed!!");
-        while (1) {
+        while (1)
+        {
             delay(5000);
         }
     }
-
 }
 
 void loop()
 {
     if (modem.getGPS(&lat2, &lon2, &speed2, &alt2, &vsat2, &usat2, &accuracy2,
-                     &year2, &month2, &day2, &hour2, &min2, &sec2)) {
+                     &year2, &month2, &day2, &hour2, &min2, &sec2))
+    {
         Serial.println();
-        Serial.print("lat:"); Serial.print(String(lat2, 8)); Serial.print("\t");
-        Serial.print("lon:"); Serial.print(String(lon2, 8)); Serial.println();
-        Serial.print("speed:"); Serial.print(speed2); Serial.print("\t");
-        Serial.print("altitude:"); Serial.print(alt2); Serial.println();
-        Serial.print("year:"); Serial.print(year2);
-        Serial.print(" montoh:"); Serial.print(month2);
-        Serial.print(" day:"); Serial.print(day2);
-        Serial.print(" hour:"); Serial.print(hour2);
-        Serial.print(" minutes:"); Serial.print(min2);
-        Serial.print(" second:"); Serial.print(sec2); Serial.println();
+        Serial.print("lat:");
+        Serial.print(String(lat2, 8));
+        Serial.print("\t");
+        Serial.print("lon:");
+        Serial.print(String(lon2, 8));
+        Serial.println();
+        Serial.print("speed:");
+        Serial.print(speed2);
+        Serial.print("\t");
+        Serial.print("altitude:");
+        Serial.print(alt2);
+        Serial.println();
+        Serial.print("year:");
+        Serial.print(year2);
+        Serial.print(" montoh:");
+        Serial.print(month2);
+        Serial.print(" day:");
+        Serial.print(day2);
+        Serial.print(" hour:");
+        Serial.print(hour2);
+        Serial.print(" minutes:");
+        Serial.print(min2);
+        Serial.print(" second:");
+        Serial.print(sec2);
+        Serial.println();
         Serial.println();
 
         // After successful positioning, the PMU charging indicator flashes quickly
         PMU.setChargingLedMode(XPOWERS_CHG_LED_BLINK_4HZ);
-        while (1) {
+        while (1)
+        {
             delay(1000);
         }
-    } else {
+    }
+    else
+    {
         // Blinking PMU charging indicator
         PMU.setChargingLedMode(level ? XPOWERS_CHG_LED_ON : XPOWERS_CHG_LED_OFF);
         level ^= 1;
